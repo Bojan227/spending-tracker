15ea23f4ba917ebe80a6d30e97ce2b52a0fb3219
Bojan227 <blazevski.bojan1@gmail.com>
2023-05-17 15:29:40 +0200

    add useGetUserById custom hook

---
+++ app/hooks/getUserById.ts
@@ -0,0 +1,24 @@
+import { db } from "@/app/firebase";
+import { useQuery } from "@tanstack/react-query";
+import { getDoc, doc } from "firebase/firestore";
+
+const getUserById = async (userId: string) => {
+  const userRef = doc(db, "users", userId);
+  const userDoc = await getDoc(userRef);
+
+  if (userDoc.exists()) {
+    return userDoc.data();
+  } else {
+    throw new Error("User not found");
+  }
+};
+
+export default function useGetUserById(userId: string) {
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ["users", userId],
+    queryFn: async () => getUserById(userId),
+    enabled: Boolean(userId),
+  });
+
+  return { isLoading, isError, error, data };
+}
